086394e984dd696a66950ed2dc8c33f468d60383
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-09-20 17:19:32 +0300

    fix
    uncomment cmd in dockerfile, remove drop metadata

--- app/db/db_connect.py
+++ app/db/db_connect.py
@@ -15,7 +15,6 @@ Base = declarative_base()
 
 async def init_models():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
