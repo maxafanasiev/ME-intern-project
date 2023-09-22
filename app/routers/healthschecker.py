9811dea8d558ed497b9f0f11815f1fc789cb1948
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-09-22 21:46:29 +0300

    add classes in services

--- app/routers/healthschecker.py
+++ app/routers/healthschecker.py
@@ -1,29 +1,50 @@
 from fastapi import APIRouter
-from app.services.healthchecker_funcs import check_redis, check_postgres_db, create_response
+from app.services.healthchecker_funcs import PostgresStatusChecker, RedisStatusChecker
 
 router = APIRouter(tags=['healthchecker'])
 
 
 @router.get("/")
 async def root():
-    return await create_response(200, "ok", "working")
+    return {
+        "status_code": 200,
+        "detail": "ok",
+        "result": "working"
+    }
 
 
 @router.get("/db_health")
 async def check_health():
-    postgres_result = await check_postgres_db()
+    postgres_checker = PostgresStatusChecker()
+    postgres_status_result = await postgres_checker.check_status()
 
-    if postgres_result["postgres_status"] == "ok":
-        return await create_response(200, "ok", "working")
-    return await create_response(500, "Internal Server Error", "not working")
+    if postgres_status_result:
+        return {
+            "status_code": 200,
+            "detail": "ok",
+            "result": "working"
+        }
+    return {
+        "status_code": 500,
+        "detail": "Internal Server Error",
+        "result": "not working"
+    }
 
 
 @router.get("/redis_health")
 async def check_health():
-    redis_result = await check_redis()
+    redis_checker = RedisStatusChecker()
+    redis_status_result = await redis_checker.check_status()
 
-    if redis_result["redis_status"] == "ok":
-        return await create_response(200, "ok", "working")
-    return await create_response(500, "Internal Server Error", "not working")
+    if redis_status_result:
+        return {
+            "status_code": 200,
+            "detail": "ok",
+            "result": "working"
+        }
 
-
+    return {
+        "status_code": 500,
+        "detail": "Internal Server Error",
+        "result": "not working"
+    }
