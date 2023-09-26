fd7cb80b2005b04d788774488e85742df6f04e5d
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-09-26 12:25:17 +0300

    fix async

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,10 +21,3 @@ def test_redis_heath(test_client):
     response = test_client.get("/redis_health/")
     assert response.status_code == 200
 
-
-def test_password_hashing(test_client):
-    password = fake.password(length=random.randint(8, 16))
-    hashed_password = auth_service.get_password_hash(password)
-    verify_password = auth_service.verify_password(password, hashed_password)
-    assert verify_password is True
-
