f8276cf52f259fae191573a0175efdd16af4d926
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-09-26 11:56:34 +0300

    Update healthschecker.py

--- app/routers/healthschecker.py
+++ app/routers/healthschecker.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from app.services.healthchecker_services import PostgresStatusChecker, RedisStatusChecker
 
 router = APIRouter(tags=['healthchecker'])
@@ -14,8 +14,7 @@ async def root():
 
 
 @router.get("/db_health")
-async def check_health():
-    postgres_checker = PostgresStatusChecker()
+async def check_health(postgres_checker: PostgresStatusChecker = Depends(PostgresStatusChecker)):
     postgres_status_result = await postgres_checker.check_status()
 
     if postgres_status_result:
@@ -24,27 +23,18 @@ async def check_health():
             "detail": "ok",
             "result": "working"
         }
-    return {
-        "status_code": 500,
-        "detail": "Internal Server Error",
-        "result": "not working"
-    }
+    raise HTTPException(status_code=500, detail="Postgres Connection Error")
 
 
 @router.get("/redis_health")
-async def check_health():
-    redis_checker = RedisStatusChecker()
+async def check_health(redis_checker: RedisStatusChecker = Depends(RedisStatusChecker)):
     redis_status_result = await redis_checker.check_status()
 
-    if redis_status_result:
+    if redis_status_result is True:
         return {
             "status_code": 200,
             "detail": "ok",
             "result": "working"
         }
 
-    return {
-        "status_code": 500,
-        "detail": "Internal Server Error",
-        "result": "not working"
-    }
+    raise HTTPException(status_code=500, detail="Redis Connection Error")
