ab2c7c19d1f20cfe4251026cb9454e5f2c12cdc8
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-09-26 13:44:32 +0300

    create user_repository

--- app/routers/users.py
+++ app/routers/users.py
@@ -12,65 +12,33 @@ from app.services.users_services import UserServices
 from app.services.auth_services import auth_service
 from app.services.pagination import Paginator
 from app.core.logger import logger
+from app.repository import users_repository
 
 router = APIRouter(tags=["users"])
 
 
 @router.post("/signup", response_model=UserModel, status_code=status.HTTP_201_CREATED)
 async def create_user(body: SignUpRequestModel, db: AsyncSession = Depends(get_db)):
-    async with db as session:
-        exist_user = await UserServices.get_user_by_email(body.user_email, session)
-        if exist_user:
-            logger.error(f"Error create user")
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists")
-        body.password = await auth_service.get_password_hash(body.password)
-        new_user = await UserServices.create_user(body, session)
-        return new_user
+    db_user = await users_repository.create_user(body, db)
+    return db_user
 
 
 @router.get("/{user_id}", response_model=UserDetailResponse)
 async def read_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    async with db as session:
-        stmt = select(User).where(User.user_id == user_id)
-        result = await session.execute(stmt)
-        db_user = result.scalar_one_or_none()
-        if db_user is None:
-            logger.error(f"Error get user by id")
-            raise HTTPException(status_code=404, detail="User not found")
-        return db_user
+    user = await users_repository.get_user_by_id(user_id, db)
+    return user
 
 
 @router.put("/{user_id}", response_model=UserDetailResponse)
 async def update_user(user_id: int, user: UserUpdateRequestModel, db: AsyncSession = Depends(get_db)):
-    async with db as session:
-        stmt = select(User).where(User.user_id == user_id)
-        result = await session.execute(stmt)
-        db_user = result.scalar_one_or_none()
-        if db_user is None:
-            logger.error(f"Error updating user")
-            raise HTTPException(status_code=404, detail="User not found")
-        db_user.password = await auth_service.get_password_hash(db_user.password)
-        db_user.updated_at = datetime.now()
-        for field, value in user.model_dump().items():
-            if value is not None:
-                setattr(db_user, field, value)
-        await session.commit()
-        await session.refresh(db_user)
-        return db_user
+    db_user = await users_repository.update_user_by_id(user_id, user, db)
+    return db_user
 
 
 @router.delete("/{user_id}", response_model=UserModel)
 async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    async with db as session:
-        stmt = select(User).where(User.user_id == user_id)
-        result = await session.execute(stmt)
-        db_user = result.scalar_one_or_none()
-        if db_user is None:
-            logger.error(f"Error delete user")
-            raise HTTPException(status_code=404, detail="User not found")
-        await session.delete(db_user)
-        await session.commit()
-        return db_user
+    db_user = await users_repository.delete_user_by_id(user_id, db)
+    return db_user
 
 
 @router.get("/", response_model=UsersListResponse)
