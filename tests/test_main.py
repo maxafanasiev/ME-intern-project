76a1567515bf52c73ace6e31b773a629f10b0fb0
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-09-26 12:18:30 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 import random
+import asyncio
 
 from faker import Faker
 
@@ -20,11 +21,3 @@ def test_db_heath(test_client):
 def test_redis_heath(test_client):
     response = test_client.get("/redis_health/")
     assert response.status_code == 200
-
-
-def test_password_hashing(test_client):
-    password = fake.password(length=random.randint(8, 16))
-    hashed_password = auth_service.get_password_hash(password)
-    verify_password = auth_service.verify_password(password, hashed_password)
-    assert verify_password is True
-
