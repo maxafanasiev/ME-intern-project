7b1634e1de4b0f5b57b22c94fe1945d1b714cefb
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-09-30 23:35:17 +0300

    Update companies.py

--- app/services/companies.py
+++ app/services/companies.py
@@ -6,21 +6,21 @@ from app.db.models import User as UserModel
 
 class CompanyService:
     def __init__(self, company_repo: AbstractRepository):
-        self.user_repo: AbstractRepository = company_repo()
+        self.company_repo: AbstractRepository = company_repo()
 
     async def create_company(self, company: CreateCompanyRequestModel,
                              current_user: UserModel) -> CompanyDetailResponse:
-        return await self.user_repo.add_one(company, current_user)
+        return await self.company_repo.add_one(company, current_user)
 
     async def get_company_by_id(self, company_id: int) -> CompanyDetailResponse:
-        return await self.user_repo.get_one(company_id)
+        return await self.company_repo.get_one(company_id)
 
     async def get_all_companies(self, page: int, size: int) -> CompanyListResponse:
-        return await self.user_repo.get_all(page, size)
+        return await self.company_repo.get_all(page, size)
 
     async def update_company(self, company_id: int, company: CompanyUpdateRequestModel,
                              current_user: UserModel) -> CompanyDetailResponse:
-        return await self.user_repo.update_one(company_id, company, current_user)
+        return await self.company_repo.update_one(company_id, company, current_user)
 
     async def delete_company(self, company_id: int, current_user: UserModel) -> CompanyDetailResponse:
-        return await self.user_repo.delete_one(company_id, current_user)
+        return await self.company_repo.delete_one(company_id, current_user)
