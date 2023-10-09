b0b3383938f71a37dcae2651b2ac12232d11d92f
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-10-10 01:12:33 +0300

    Update notifications_repo.py

--- app/repository/notifications_repo.py
+++ app/repository/notifications_repo.py
@@ -9,13 +9,13 @@ class NotificationRepository:
     async def create_quiz_notification(self, user_id, quiz_id, session):
         user = await session.get(User, user_id)
         quiz = await session.get(Quiz, quiz_id)
-        if user and quiz:
-            notification = Notification(
-                user_id=user_id,
-                text=f"Dear User. We invite you to complete a new quizz: {quiz.quiz_name}"
-            )
-            session.add(notification)
-            await session.commit()
-            return notification
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User or quiz not found")
+        if not (user and quiz):
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User or quiz not found")
+        notification = Notification(
+            user_id=user_id,
+            text=f"Dear User. We invite you to complete a new quizz: {quiz.quiz_name}"
+        )
+        session.add(notification)
+        await session.commit()
+        return notification
 
